6273d3b932b10a068dcddfe55c519718416c4d1e
adarsh <chateadarsh7777@gmail.com>
2024-01-10 00:13:47 +0530

    test commit 2

--- jdbcPostgres/src/DemoJdbc.java
+++ jdbcPostgres/src/DemoJdbc.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class DemoJdbc {
+import java.sql.*;
+
+public class DemoJdbc {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Import Package
+         * load and register
+         * create connection
+         * create statement
+         * execute statement
+         * process the result
+         * close
+         */
+
+        String url="jdbc:postgresql://localhost:5432/demoDB";
+        String uname="postgres";
+        String pass="pass";
+
+//        Class.forName("org.postgresql.Driver");  /// not compulsory
+        Connection con = DriverManager.getConnection(url, uname, pass);
+
+        System.out.println("Connection Established");
+
+    }
 }
